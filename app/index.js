60903762a5ffa6b7a5d9b174d3b4b9d2664529ad
Jake <jake83@gmail.com>
2016-03-30 18:59:44 -0700

    fix path for heroku

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res){
-  res.send(200, 'OK'); 
+  res.send(200, 'OK');
 });
 
 app.listen(app.get('port'), function() {
