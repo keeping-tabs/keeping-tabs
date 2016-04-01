870de9412d7c8ec25ac1e4c3afa11cb4710cede1
Justin Lim <liminator1106@gmail.com>
2016-04-01 14:06:48 -0700

    worked on queue and created timer

--- chrome_ext/Queue.js
+++ chrome_ext/Queue.js
@@ -1,5 +1,6 @@
 var Queue = function(){
 	this.structure = {};
+	this.hashTable = {};
 	this.first = -1;
 	this.last = -1;
 };
@@ -16,19 +17,32 @@ Queue.prototype.findFirst = function(index){
 };
 
 Queue.prototype.delete = function(key){
-	// use key to search thru hash table for url
-	
-	delete this.structure[key];
-}
+	var index = this.hashTable[key]
+	delete this.hashTable[index];
+	delete this.structure[index];
+};
 
-Queue.prototype.enqueue = function(url){
+Queue.prototype.enqueue = function(key, data){
 	this.last++;
-	this.structure[this.last] = url;
+	this.structure[this.last] = {key: key, data: data};
+	this.hashTable[key] = this.last;
+	if(this.first === -1){
+		this.first = this.last;
+	}
 };
 
 Queue.prototype.dequeue = function(){
+	if(this.first === -1){
+		return null;
+	}
 	var output = this.structure[this.first];
-	this.delete(this.first);
+	this.delete(output.key);
 	this.first = this.findFirst(this.first);
 	return output;
+};
+
+Queue.prototype.update = function(key, data){
+	var index = this.hashTable[key];
+	this.delete(key);
+	this.enqueue(key, data);
 };
