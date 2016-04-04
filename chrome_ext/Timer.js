b3de97590aea59ffe79616c8adaa07622210b3f7
Justin Lim <liminator1106@gmail.com>
2016-04-04 10:58:15 -0700

    added timer

--- chrome_ext/Timer.js
+++ chrome_ext/Timer.js
@@ -4,16 +4,20 @@ var removeTab = function(){
 };
 
 var timer;
+//timer is set here for now because I want access to it on my initializeTimer function
+
 var userTimeLimit;
+//userTimeLimit will be set by user somewhere
 
 var initializeTimer = function(){
 	if(timer){
 		clearTimeout(timer);
 	}
-	if(this.first === -1){
+	if(this.first === -1){x
 		return null;
 	}
 	var elapsedTime = (new Date()).getTime() - this.structure[this.first].data.createdAt;
+	//I'm naming it queue.structure[this.first].data.createdAt for now, can be named something else eventually
 	var timeRemaining = userTimeLimit - elapsedTime;
 	if(timeRemaining <= 0){
 		removeTab.call(this);
