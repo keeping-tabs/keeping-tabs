97cb6269a1713adfc84e82bc1979efc08d2b66f5
louisbuchbinder <louisbuchbinder@gmail.com>
2016-04-05 08:52:14 -0700

    linting files

--- app/server/spec/serverSpecRequired_request.js
+++ app/server/spec/serverSpecRequired_request.js
@@ -3,11 +3,7 @@ var http = require('http');
 
 var collectData = function (response) {
   return new Promise(function (resolve, reject) {
-<<<<<<< f12b5d03e20e958e969918475482b93982d0b469
     var data = '';
-=======
-    data = '';
->>>>>>> chai as promised async tests begun
     response.on('data', function (chunk) {
       // console.log(chunk);
       data += chunk;
@@ -25,11 +21,7 @@ var collectData = function (response) {
 
 // requestOptions = {host:'localhost' , port: '8080', path: '/', method, data,headers:{'custom': 'Custom Header Demo works'}} pseudocode
 
-<<<<<<< f12b5d03e20e958e969918475482b93982d0b469
 var request = function (options) {
-=======
-module.exports = request = function (options) {
->>>>>>> chai as promised async tests begun
   if (typeof options !== 'object') { 
     throw new Error('options argument must be an object!');
   }
@@ -47,10 +39,8 @@ module.exports = request = function (options) {
   });
 };
 
-<<<<<<< f12b5d03e20e958e969918475482b93982d0b469
 module.exports = request;
-=======
->>>>>>> chai as promised async tests begun
+
 
 // request({host:'localhost' , port: '8080', path: '/', method: 'GET'})
 // .then(function (data) {
