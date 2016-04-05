8bdb008120db90879f6a76ffaa7c5b38779c81c9
Justin Lim <liminator1106@gmail.com>
2016-04-05 15:05:58 -0700

    extension files moved around and listeners in place

--- app/chrome/scripts/algorithm/listeners.js
+++ app/chrome/scripts/algorithm/listeners.js
@@ -1,4 +1,5 @@
 //assumes instance of Queue exists
+var queue = new Queue();
 
 chrome.tabs.onCreated.addListener(function(tab){
 	// send tab to queue
@@ -7,17 +8,19 @@ chrome.tabs.onCreated.addListener(function(tab){
 		url: tab.url,
 		createdAt: createdAt
 	};
+	console.log(tab);
 	queue.enqueue(tab.id, data);
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo){
+chrome.tabs.onUpdated.addListener(function(tabId){
 	// look for relevant changed info in changeInfo (object), create (new Date()).getTime(), and run queue.update() with the updated info
   	chrome.tabs.get(tabId, function(tab){
   		var createdAt = (new Date()).getTime();
   		var data = {
   			url: tab.url,
   			createdAt: createdAt
-  		}
+  		};
+  		console.log(tab);
   		queue.update(tabId, data);
   	});
 	//if queue.first was pointing to updated tab, run initializeTimer()
@@ -26,15 +29,16 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo){
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
 	// find tab in queue using tabId (given in activeInfo),
+  var tabId = activeInfo.tabId;
   chrome.tabs.get(tabId, function(tab){
     var createdAt = (new Date()).getTime();
-    var tabId = activeInfo.tabId;
     var data = {
     	url: tab.url,
     	createdAt: createdAt
-    }
-    queue.update(tabId, data)
-  })
+    };
+    console.log('hello from here', tab);
+    queue.update(tabId, data);
+  });
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId){
