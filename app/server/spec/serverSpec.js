5a1915ef10a625dfa0745f80b99a5ac7bced0756
louisbuchbinder <louisbuchbinder@gmail.com>
2016-04-04 19:01:33 -0700

    chai as promised async tests begun

--- app/server/spec/serverSpec.js
+++ app/server/spec/serverSpec.js
@@ -1,39 +1,28 @@
-var foo = 'bar';
-var tea = {
-  flavors:['mocha', 'chai', 'coffee']
-};
 
 var chai = require('chai');
 var assert = require('assert');
+
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+var request = require('./serverSpecRequired_request');
+
+chai.should();
+var expect = chai.expect;
+var assert = chai.assert;
+
+
+
 /* globals describe: false, it: false */
-describe('Example Tests', function () {
-  it('should equal 2 in just plain mocha', function () {
-    assert.equal(2, 1 + 1);
-  });
-  it('An example of using chai should', function () {
-    chai.should();
-    foo.should.be.a('string');
-    foo.should.equal('bar');
-    foo.should.have.length(3);
-    tea.should.have.property('flavors')
-      .with.length(3);
-  });
-  it('An example of using chai expect', function () {
-    var expect = chai.expect;
+describe('Server Tests', function () {
+  it('should respond to get requests at /', function () {
+    return expect(new Promise(function (resolve, reject) {
+      request({host: 'localhost', port: '8080', path: '/', method: 'GET'})
+      .then(function (data) {resolve(data.statusCode)});
+    })).to.eventually.equal(200);
 
-    expect(foo).to.be.a('string');
-    expect(foo).to.equal('bar');
-    expect(foo).to.have.length(3);
-    expect(tea).to.have.property('flavors')
-      .with.length(3);
   });
-  it('An example of using chai assert', function () {
-    var assert = chai.assert;
 
-    assert.typeOf(foo, 'string');
-    assert.equal(foo, 'bar');
-    assert.lengthOf(foo, 3);
-    assert.property(tea, 'flavors');
-    assert.lengthOf(tea.flavors, 3);
-  });
+
 });
---
+++ app/server/spec/serverSpecRequired_request.js
@@ -0,0 +1,48 @@
+var http = require('http');
+
+
+var collectData = function (response) {
+  return new Promise(function (resolve, reject) {
+    data = '';
+    response.on('data', function (chunk) {
+      // console.log(chunk);
+      data += chunk;
+    });
+    response.on('end', function () {
+      resolve({statusCode: response.statusCode, data: String(data)});
+    });
+    response.on('error', function (error) {
+      reject(error);
+    });
+  });
+};
+
+
+
+// requestOptions = {host:'localhost' , port: '8080', path: '/', method, data,headers:{'custom': 'Custom Header Demo works'}} pseudocode
+
+module.exports = request = function (options) {
+  if (typeof options !== 'object') { 
+    throw new Error('options argument must be an object!');
+  }
+  return new Promise(function (resolve, reject) {
+    var request = http.request(options, function (response) {
+      // console.log(response);
+      collectData(response)
+        .then(resolve)
+      .catch(reject);
+    });
+    if (options.data) {
+      request.write(options.data);
+    }
+    request.end();
+  });
+};
+
+
+// request({host:'localhost' , port: '8080', path: '/', method: 'GET'})
+// .then(function (data) {
+//   console.log('data: ', data);
+// });
+
+
