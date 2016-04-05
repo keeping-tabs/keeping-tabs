29ce41ee069e074112580e8815812c98bb0deffb
Jake <jake83@gmail.com>
2016-04-05 12:27:55 -0700

    simpler server test

--- app/server/spec/serverSpec.js
+++ app/server/spec/serverSpec.js
@@ -1,26 +1,21 @@
-// mocha + chai testing requirements
-var chai = require('chai');
-// var assert = require('assert');//load mocha assert
-var chaiAsPromised = require('chai-as-promised');
-// mocha + chai testing middleware
-chai.use(chaiAsPromised);
-chai.should();
-var expect = chai.expect;
-// assert = chai.assert;//uncomment to use chai assert
-// http requests node helper
-var request = require('./serverSpecRequired_request');
+var expect = require('chai').expect;
+var server = require('../../index.js');
+var supertest = require('supertest');
 
+var request = supertest.agent(server);
 
-/* globals describe: false, it: false */
-describe('Server Tests', function () {
-  it('should respond to get requests at /', function () {
-    return expect(new Promise(function (resolve, reject) {
-      request({host: 'localhost', port: '8080', path: '/', method: 'GET'})
-      .then(function (data) {resolve(data.statusCode);})
-      .catch(reject);
-    })).to.eventually.equal(200);
-
+describe('server', function() {
+  describe('GET /', function() {
+    it('should return html of homepage', function() {
+      request
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', /html/)
+        .expect(/Keeping Tabs: Chrome Extension/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        }); // just checking the existence of these text
+    });
   });
-
-
 });
