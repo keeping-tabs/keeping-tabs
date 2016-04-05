b17524f517bca545601911b87078379a5eace6e6
Jake <jake83@gmail.com>
2016-04-04 20:00:42 -0700

    remove trailing slash

--- app/server/configuration.js
+++ app/server/configuration.js
@@ -15,7 +15,7 @@ module.exports = app;
 /////*****/////*****/////*****/////*****/////*****
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'chrome-extension://amaekhdmilmhgmoaackfphcjclhghmfe/');
+    res.header('Access-Control-Allow-Origin', 'chrome-extension://amaekhdmilmhgmoaackfphcjclhghmfe');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
