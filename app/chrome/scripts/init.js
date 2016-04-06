cf71386750ec0ebbc4d88da68e726aed0611a7e6
Jake <jake83@gmail.com>
2016-04-06 13:41:07 -0700

    Server tests
    * add reporter for frontend tests
    
    * move all dev dep to dep, easier for heroku
    
    * simpler server test
    
    * add more tests for sever
    
    * jshint
    
    * use browserify instead of concat

--- app/chrome/scripts/init.js
+++ app/chrome/scripts/init.js
@@ -1,38 +1,39 @@
-(function () {
-  /* globals $: false */
-  $(function(){
-    /* globals chrome: false */
-    console.log('Service Initialized');
 
-    chrome.browserAction.onClicked.addListener(function(){
-      getTab(postTabs);
-    });
+var queue = require('./algorithm/Queue.js');
 
-    function getTab(callback){
-      chrome.tabs.query({
-        active: true,
-        currentWindow: true
-      }, function(tabArray){
-        var tab = tabArray[0];
-        var url = [tab.url];
-        
-        console.log(url);
+/* globals $: false */
+$(function(){
+  /* globals chrome: false, ENV: false */
+  console.log('Service Initialized');
 
-        callback(url).then(console.log('success'));
-      });
-    }
-
-    function postTabs(urls) {
-      // sending object
-      return new Promise(function(resolve,reject){
-        $.ajax({
-          type: 'POST',
-          url: ENV.url + '/links',
-          data: {urls: urls},
-          success: resolve
-        })
-        .fail(reject);
-      });
-    }
+  chrome.browserAction.onClicked.addListener(function(){
+    getTab(postTabs);
   });
-})();
+
+  function getTab(callback){
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabArray){
+      var tab = tabArray[0];
+      var url = [tab.url];
+      
+      console.log(url);
+
+      callback(url).then(console.log('success'));
+    });
+  }
+
+  function postTabs(urls) {
+    // sending object
+    return new Promise(function(resolve,reject){
+      $.ajax({
+        type: 'POST',
+        url: ENV.url + '/links',
+        data: {urls: urls},
+        success: resolve
+      })
+      .fail(reject);
+    });
+  }
+});
