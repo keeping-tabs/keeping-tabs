2831abd8db0d7ec1ca772d0be63e3485fc245db8
Jake <jake83@gmail.com>
2016-04-06 12:06:03 -0700

    add more tests for sever

--- app/server/spec/serverSpec.js
+++ app/server/spec/serverSpec.js
@@ -4,9 +4,11 @@ var supertest = require('supertest');
 
 var request = supertest.agent(server);
 
+/*global beforeEach, afterEach, describe, expect, it, spyOn, xdescribe, xit */
+
 describe('server', function() {
   describe('GET /', function() {
-    it('should return html of homepage', function() {
+    it('Should return html of homepage', function(done) {
       request
         .get('/')
         .expect(200)
@@ -16,6 +18,50 @@ describe('server', function() {
           if (err) return done(err);
           done();
         }); // just checking the existence of these text
+    });
+  });
+
+  describe('Serve Chrome Extension Scripts', function() {
+    
+    function isJavaScript(res) {
+      try {
+        eval(res.text);
+      } catch(e) {
+        if(e instanceof SyntaxError)
+          throw new Error('not javascript');
+        else {
+          return true;
+        }
+      }
+    }
+
+    it('/chrome/dist/script.js', function(done) {
+      request
+        .get('/chrome/dist/script.js')
+        .expect(isJavaScript)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    it('/chrome/dist/vendors.js', function(done) {
+      request
+        .get('/chrome/dist/vendors.js')
+        .expect(isJavaScript)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+  describe('API', function() {
+    it('POST /links', function(done) {
+      request
+        .post('/links')
+        .send({ urls: ['http://apple.com'] })
+        .expect(201, done);
     });
   });
 });
