98d808a714346e4fac08bbe0e43fb04aeac94179
Jake <jake83@gmail.com>
2016-04-07 12:49:45 -0700

    set time from popup

--- app/chrome/scripts/init.js
+++ app/chrome/scripts/init.js
@@ -1,5 +1,8 @@
 var listeners = require('./algorithm/listeners.js');
+<<<<<<< 3e050b5732fe65144025c79f29969f115f4a4ff7
 
+=======
+>>>>>>> set time from popup
 /* globals $: false */
 $(function(){
   /* globals chrome: false, ENV: false */
