9b9852c059dd09dfed8fb039219d5c942adcb517
Jake <jake83@gmail.com>
2016-04-07 14:20:24 -0700

    clean up code

--- app/chrome/scripts/algorithm/listeners.js
+++ app/chrome/scripts/algorithm/listeners.js
@@ -51,14 +51,6 @@ exports.init = function() {
           timer.initialize(queue);
         }
       });
-
-
-
-      // currentTab = dataObj.tab;
-      // if (oldTab !== null) {
-      //   queue.enqueue(String(oldTab.id), dataObj.data);
-      //   timer.initialize(queue);
-      // } 
     });
   });
 
@@ -81,37 +73,23 @@ exports.init = function() {
     .then(function (dataObj) {
       console.log('activated', dataObj.tab.id);
 
-  Chrome.updateCurrentTabs(queue)
-  .then(Chrome.getActiveTabs)
-  .then(Chrome.mapToTabIds)
-  .then(function (tabIds) {return Chrome.findOldTabId(tabIds, currentTabs);})
-  .then(function (oldTabId) {
-    console.log('oldTabId: ', oldTabId);
-    console.log('activated: ', dataObj.tab.id);
-    currentTabs[dataObj.tab.id] = dataObj;
-    if (oldTabId) {
-      var oldTab = currentTabs[oldTabId];
-      delete currentTabs[oldTabId];
+      Chrome.updateCurrentTabs(queue)
+      .then(Chrome.getActiveTabs)
+      .then(Chrome.mapToTabIds)
+      .then(function (tabIds) {return Chrome.findOldTabId(tabIds, currentTabs);})
+      .then(function (oldTabId) {
+        console.log('oldTabId: ', oldTabId);
+        console.log('activated: ', dataObj.tab.id);
+        currentTabs[dataObj.tab.id] = dataObj;
+        if (oldTabId) {
+          var oldTab = currentTabs[oldTabId];
+          delete currentTabs[oldTabId];
 
-      queue.delete(String(dataObj.tab.id));
-      queue.update(String(oldTab.tab.id), oldTab.data);
-      timer.initialize(queue);
-    }
-  });
-
-
-
-
-
-      // var oldTab = currentTab;
-      // currentTab = dataObj.tab;
-      
-      // if (oldTab && oldTab.id !== currentTab.id) {
-      //   queue.delete(String(currentTab.id));
-      //   queue.update(String(oldTab.id), dataObj.data);
-      //   timer.initialize(queue);
-      // }
-
+          queue.delete(String(dataObj.tab.id));
+          queue.update(String(oldTab.tab.id), oldTab.data);
+          timer.initialize(queue);
+        }
+      });
     });
   });
 
