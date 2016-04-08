5377f774b6d73e63fac4954c79ff0e8edb12ea7c
Jake <jake83@gmail.com>
2016-04-08 14:38:28 -0700

    pseudocode

---
+++ app/server/authHandler.js
@@ -0,0 +1,26 @@
+var db = require('./database.js');
+
+var Auth = function() {
+  return {
+    login: handleLogin,
+    signup: handleSignup
+  };
+
+  function handleLogin(req, res) {
+    
+  }
+
+  function handleSignup(req, res) {
+    // req.body.username
+    // req.body.password
+
+    // query db for the username
+      // if found, redirect to login
+    // add user to the table
+  }
+
+  // middleware
+  function auth(req, res, next) {
+
+  }
+};
