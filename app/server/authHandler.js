847ce407c28a99d106a9719b058f080e980193a6
Jake <jake83@gmail.com>
2016-04-08 18:25:34 -0700

    use new syntax

--- app/server/authHandler.js
+++ app/server/authHandler.js
@@ -9,20 +9,20 @@ var Auth = function() {
 
   function handleLogin(req, res) {
     var username = req.body.username;
-    var password =  req.body.password;
+    // var password =  req.body.password;
 
     db.fetchUserId(username).then(function(users) {
       // do check password
-      res.send(200);
+      res.sendStatus(200);
     }).catch(function(reason) {
-      res.send(500);
+      res.sendStatus(500);
       console.log('Login failed: ', reason);
     });
   }
 
   function handleSignup(req, res) {
     var username = req.body.username;
-    var password =  req.body.password;
+    // var password =  req.body.password;
 
     // query db for the username
     db.fetchUserId(username).then(function(users) {
@@ -30,18 +30,18 @@ var Auth = function() {
       if(users.length > 0) { // check if user exist
         console.warn('User '+ username + 'already exist');
         
-        res.send(422);
+        res.sendStatus(422);
 
       } else {
         db.saveUsers([username]).then(function() {
           console.log('User '+ username + 'created');
           
-          res.send(201, {token: 'faketoken'});
+          res.status(201).send({token: 'faketoken'});
 
         });
       }
     }).catch(function(reason) {
-      res.send(500);
+      res.sendStatus(500);
       console.error('Signup failed: ', reason);
     });
   }
